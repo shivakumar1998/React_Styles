64135f6847375331ece94f9c5b3b33687a30521b
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2023-12-18 20:41:55 +0530

    Session 112

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
+import { styled } from "styled-components";
 
+/* Template Literals */
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -22,7 +30,7 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p className="paragraph">
           <label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
@@ -51,7 +59,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
